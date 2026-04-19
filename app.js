017c25a04febce1c63b60592ceb523e4eb7efd9e
Tavina Chen <tavinachen.work@gmail.com>
2026-04-19 16:19:12 -0700

    final pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Tavina Chen here. This is my Continuous Delivery Pipeline Tutorial demo'));
+app.get('/', (req, res) => res.send('My 2nd change to the App to test the Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
